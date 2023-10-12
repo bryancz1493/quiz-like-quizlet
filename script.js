38a84916586c3a15e201685ed43122e560600fbf
bryancz1493 <bryancz1493@gmail.com>
2023-10-12 01:16:22 -0700

    Write function to update the timer

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 var timeInterval = 60;
-var countdownTimer = "countdown"
+var countdownTimer = "countdown";
+var startQuizButton = $('#start-button');
 
 // Start quiz button display on browser
-function startQuiz {
-    document.getElementById('time') = timeInterval;
-
+function startQuiz () {
     if (timeInterval <= 0) {
         clearInterval(countdownTimer);
         alert ("Time Up! Game Over!");
 
         return null;
-    }
+    } 
 
     timeInterval --;
 }
 
 // Timer on top right corner
+function updateTimer () {
+    document.getElementById('time') = timeInterval;
 
+    for (let i= 60; timeInterval.length <= 0; i--) {
+        return timeInterval;
+    }
+}
 
 // 15 Objective questions 
 function submitButton(event) {
